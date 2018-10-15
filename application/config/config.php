8af49705a2bee523f6ad1ee7af8c5b88eb8d53a5
root <root@ip-172-31-25-115.ec2.internal>
2018-10-15 11:25:15 +0000

    Untrack Log and enable for dynamic domain base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://52.202.87.154/';
+$domain = $_SERVER['SERVER_NAME'];
+$config['base_url'] = 'http://'.$domain.'/';
 /*
 |--------------------------------------------------------------------------
 | Index File
